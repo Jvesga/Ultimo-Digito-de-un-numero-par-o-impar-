610f65c455af51e5d706d4b3856f6177f8550d22
Jvesga <jorge2224514@correo.ui.edu.co>
2022-04-26 11:24:46 -0500

    Ultimo-digito-par-o-impar-Jvesga

---
+++ README.md
@@ -0,0 +1,4 @@
+Programa que lee un numero X y imprime su ultimo digito y tambien nos dice si el ultimo digito es Par o Impar 
+
+Diagrama de flujo
+![Diagramadeflujo](Ultimo_digito_par_o_impar.png)
---
+++ Ultimo_digito_par_o_impar.py
@@ -0,0 +1,19 @@
+"""Programa para determinar si el ultimo dgito de un numero es Par o Impar"""
+
+print("-------------------------------")
+print("---Ultimo digito Par o Impar---")
+print("_______________________________")
+
+# input
+X=int(input(" Digite un numero: "))
+# prossesing
+n = X % 10 
+
+if n % 2 == 0:
+    msj = "Par"
+else:
+    msj = "Impar"
+# prossesing
+
+print("el ultimo numero de:",X, "es",n) 
+print("Por tanto el ultimo digito de su numero es " + msj)
