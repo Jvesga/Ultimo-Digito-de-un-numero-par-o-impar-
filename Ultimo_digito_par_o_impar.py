d8d9f2927ec1826d5dd271c0982eafcf2d2407cf
Jorge Alejandro Vesga Quintero <102545608+Jvesga@users.noreply.github.com>
2022-04-26 14:12:40 -0500

    Update Ultimo_digito_par_o_impar.py

--- Ultimo_digito_par_o_impar.py
+++ Ultimo_digito_par_o_impar.py
@@ -7,13 +7,12 @@ print("_______________________________")
 # input
 X=int(input(" Digite un numero: "))
 # prossesing
-n = X % 10 
+n = X % 10
 
 if n % 2 == 0:
     msj = "Par"
 else:
     msj = "Impar"
 # ouput
-
 print("el ultimo numero de:",X, "es",n) 
 print("Por tanto el ultimo digito de su numero es " + msj)
